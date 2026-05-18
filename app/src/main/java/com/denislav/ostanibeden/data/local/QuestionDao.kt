bde8c702e0476ef920ca55cd8385669d747af280
Denislav Mihailov <dkilimperov@gmail.com>
2026-05-18 22:50:29 +0300

    QuestionDao extension

--- app/src/main/java/com/denislav/ostanibeden/data/local/QuestionDao.kt
+++ app/src/main/java/com/denislav/ostanibeden/data/local/QuestionDao.kt
@@ -1,6 +1,10 @@
 package com.denislav.ostanibeden.data.local
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface QuestionDao {
